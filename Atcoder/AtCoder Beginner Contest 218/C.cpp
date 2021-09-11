745b68c4f6c78c8bee3ab177e4aef40fe93f55e3
Soham Chaudhuri <chaudhuri.ber@gmail.com>
2021-09-11 19:56:05 +0530

    Create C.cpp

---
+++ Atcoder/AtCoder Beginner Contest 218/C.cpp
@@ -0,0 +1,135 @@
+#include<bits/stdc++.h>
+#define ll               long long int 
+#define loop(a,b,i)      for(long long int i=a;i<b;i++)
+#define cn               continue;
+#define pb               push_back
+#define db               double
+#define mp               make_pair
+#define endl             "\n"
+#define sz(x)            (ll)((x).size())
+#define lb               lower_bound
+#define ub               upper_bound
+#define f                first
+#define se               second
+#define vll              vector<ll>
+#define pll              pair<ll,ll>
+#define vpll             vector< pair<ll,ll> >
+#define all(x)           x.begin(),x.end()
+#define pi               3.14159265358979323846
+#define quick            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+const ll mod = 1e9 + 7;
+// const ll MAX  = 8000000000000000064LL;
+const ll MIN  = -8000000000000000064LL;
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+ll gcd(ll a,ll b) {if(b==0)return a; return gcd(b,a%b);}
+ll lcm(ll x, ll y) { ll res = x / gcd(x, y); return (res * y);}
+// ll nCr(ll n,ll r){ll ans=fact[n]; ans*=mod_inv(fact[n-r]);ans%=mod; ans*=mod_inv(fact[r]);ans%=mod; return ans;}
+using namespace std;
+ 
+const int dx[] = {-1,1,0,0,1,1,-1,-1};
+const int dy[] = {0,0,1,-1,-1,1,-1,1};
+
+signed main() 
+{ quick; 
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  
+    int tc = 1;
+    // cin>>tc;
+    while(tc--)
+    {
+        ll n; cin>>n;
+        vector<vector<char>> a1(n,vector<char>(n));
+        vector<vector<char>> b1(n,vector<char>(n));
+        vector<vector<char>> b;
+        ll i_min = INT_MAX, j_min = INT_MAX;
+        ll i_max=0, j_max=0;
+        loop(0,n,i)
+        {
+            loop(0,n,j)
+            {
+                cin>>a1[i][j];
+            }
+        }
+        loop(0,n,i)
+        {
+            loop(0,n,j)
+            {
+                cin>>b1[i][j];
+                if(b1[i][j]=='#')
+                {
+                    i_min = min(i_min,i);
+                    i_max = max(i_max,i);
+                    j_min = min(j_min,j);
+                    j_max = max(j_max,j);
+                }
+            }
+        }
+        for(ll i=i_min;i<=i_max;i++)
+        {
+            vector<char> tmp;
+            for(ll j=j_min;j<=j_max;j++)
+            {
+                tmp.pb(b1[i][j]);
+            }
+            b.pb(tmp);
+        }
+        bool ok = false;
+        loop(0,4,k)
+        {
+            for(ll i = 0; i < n / 2; i++) 
+            {
+                for(ll j = i; j < n - i - 1; j++) 
+                {
+                    ll temp = a1[i][j];
+                    a1[i][j] = a1[n - 1 - j][i];
+                    a1[n - 1 - j][i] = a1[n - 1 - i][n - 1 - j];
+                    a1[n - 1 - i][n - 1 - j] = a1[j][n - 1 - i];
+                    a1[j][n - 1 - i] = temp;
+                }
+            }
+            i_min = INT_MAX; i_max = 0;
+            j_min = INT_MAX; j_max = 0;
+            loop(0,n,i)
+            {
+                loop(0,n,j)
+                {
+                    if(a1[i][j]=='#')
+                    {
+                        i_min = min(i_min,i);
+                        i_max = max(i_max,i);
+                        j_min = min(j_min,j);
+                        j_max = max(j_max,j);
+                    }
+                }
+            }
+            vector<vector<char>> a;
+            for(ll i=i_min;i<=i_max;i++)
+            {
+                vector<char> tmp;
+                for(ll j=j_min;j<=j_max;j++)
+                {
+                    tmp.pb(a1[i][j]);
+                }
+                a.pb(tmp);
+            }
+            if(a==b)
+            {
+                ok = true;
+                break;
+            }
+        }
+        if(ok) cout<<"Yes";
+        else cout<<"No";
+    }
+ 
+cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+return 0;
+}
