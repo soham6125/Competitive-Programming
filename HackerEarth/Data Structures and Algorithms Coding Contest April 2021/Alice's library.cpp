40b1cbeada598f40c3c7975ff89039b392a327af
Soham Chaudhuri <chaudhuri.ber@gmail.com>
2021-09-16 23:04:13 +0530

    Update Alice's library.cpp

--- HackerEarth/Data Structures and Algorithms Coding Contest April 2021/Alice's library.cpp
+++ HackerEarth/Data Structures and Algorithms Coding Contest April 2021/Alice's library.cpp
@@ -1,1 +1,91 @@
-
+#include<bits/stdc++.h>
+#include <cmath>
+#include <vector>
+#define ll               long long int 
+#define loop(a,b,i)      for(long long int i=a;i<b;i++)
+#define loopr(a,b,i)     for(long long int i=a-1;i>=b;i--)
+#define pb               push_back
+#define db               double
+#define mp               make_pair
+#define endl             "\n"
+#define f                first
+#define se               second
+#define vll              vector<ll>
+#define pll              pair<ll,ll>
+#define vpll             vector< pair<ll,ll> >
+#define all(x)           x.begin(),x.end()
+#define quick            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+const ll mod  = 1000000007;
+const ll MAX  = 8000000000000000064LL;
+const ll MIN  = -8000000000000000064LL;
+ll add(ll x, ll y) {ll res = x + y; return (res >= mod ? res - mod : res);}
+ll mul(ll x, ll y) {ll res = x * y; return (res >= mod ? res % mod : res);}
+ll sub(ll x, ll y) {ll res = x - y; return (res < 0 ? res + mod : res);}
+ll power(ll x, ll y) {ll res = 1; x %= mod; while (y) {if (y & 1)res = mul(res, x); y >>= 1; x = mul(x, x);} return res;}
+ll mod_inv(ll x) {return power(x, mod - 2);}
+ll gcd(ll a,ll b) {if(b==0)return a; return gcd(b,a%b);}
+ll lcm(ll x, ll y) { ll res = x / gcd(x, y); return (res * y);}
+// ll nCr(ll n,ll r){ ll ans=fact[n]; ans*=mod_inv(fact[n-r]);ans%=mod; ans*=mod_inv(fact[r]);ans%=mod; return ans;}
+using namespace std;
+ 
+bool isprime(ll n){  
+    if(n<=1) return false; 
+    if(n==2) return true;
+    if(n%2==0 and n!=2) return false;
+    for(ll i=2;i<=sqrt(n);i++) 
+        if(n%i==0) 
+            return false; 
+    return true; 
+} 
+ 
+bool isPowerOfTwo(ll n){
+   if(n==0) return false; 
+   return (ceil(log2(n)) == floor(log2(n)));  
+}
+ 
+string solve(string &s)
+{
+  ll n = s.length();
+  ll cnt = 0;
+  loop(0,n,i)
+  {
+    if(s[i] == 92)
+    {
+      ll j = i;
+      while(j>=0 and s[j]!=47)
+        j--;
+      string ans;
+      ans += s.substr(0,j);
+      string tmp = s.substr(j+1,i-j-1);
+      reverse(all(tmp));
+      ans += tmp;
+      ans += s.substr(i+1,n-i-1);
+      s = ans;
+      cnt++;
+      break;
+    }
+  }
+  if(cnt==0)
+    return s;
+  return solve(s);
+}
+ 
+signed main() 
+{ quick;  
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+ 
+    int tc = 1;
+    // cin>>tc;
+    while(tc--)
+    { 
+      string s;
+      cin >> s;
+      cout << solve(s);
+    }
+ 
+cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+}
