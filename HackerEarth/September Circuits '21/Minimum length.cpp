3e2df4439520ce73e839fd63299f1b9e96bd0de8
Soham Chaudhuri <chaudhuri.ber@gmail.com>
2021-09-26 12:45:49 +0530

    UPDATE

--- HackerEarth/September Circuits '21/Minimum length.cpp
+++ HackerEarth/September Circuits '21/Minimum length.cpp
@@ -9,8 +9,12 @@ signed main()
     int t; cin>>t; 
     while(t--)
     {   
-        int n; 
-        cin>>n;
+        // let x be number of a's and (n-x) be the number of b's
+        // total number of distinct substrings with only a's is x
+        // total number of distinct substrings with only b's is (n-x)
+        // total number of distinct substrings with both a's and b's are x*(n-x) 
+        // differentiate [x + (n-x) + x*(n-x)]>=sum w.r.t x
+        int n; cin>>n;
         double b_count = ((double)sqrt(4 + 4*n) + (-1)*2 );
         b_count /= 2;
         double ans = (n + b_count*b_count) / (1 + b_count);
